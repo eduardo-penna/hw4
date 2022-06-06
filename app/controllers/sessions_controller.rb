4d2eb21d7ac8e5a52c42117dc95cf790efcd5044
eduardo-penna <eduardo.penna@kellogg.northwestern.edu>
2022-06-06 02:38:39 +0000

    sessions controller cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if @user 
       if BCrypt::Password.new(@user.password) == params["password"]
-        session["user_id"] = @user.id 
+        session["user_id"] = @user["id"]
         flash[:notice] = "Welcome! You are logged in!"
         redirect_to "/places"
       else 
